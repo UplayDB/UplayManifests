c6f93655c3a6f9df2ee37f3eba753add757daee7
SlejmUr <slejmur@protonmail.com>
2023-03-19 18:08:21 +0100

    manifestlist & some of the older manifests

---
+++ csv_to_list.py
@@ -0,0 +1,22 @@
+import os
+
+csvlist = os.listdir("files")
+
+csv_list = {}
+
+for csvs in csvlist:
+    if csvs.__contains__(".csv"):
+        csplit = csvs.split('_')
+        x = csv_list.get(str(csplit[0]))
+        if x is None:
+            csv_list[str(csplit[0])] = []
+            csv_list[str(csplit[0])].append(csplit[1].replace(".csv",""))
+        else:
+            csv_list[str(csplit[0])].append(csplit[1].replace(".csv",""))
+
+for csvv in csv_list:
+    fp = open("manifestslist\\"+csvv + '.txt', 'w')
+    fp.write(str(csv_list[csvv]))
+    fp.close()
+
+print("Done!")    
